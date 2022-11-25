9899adf4c1f870e8441261fd83e943903215ea18
IsukaChiyogami <54658108+IsukaChiyogami@users.noreply.github.com>
2022-11-25 23:00:31 +0800

    Add files via upload

---
+++ w_plugin_2.x.x/web_code/popup.js
@@ -0,0 +1,52 @@
+
+function sendMessageToContentScript(message)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		message.tabId=tabs[0].id;
+		chrome.tabs.sendMessage(tabs[0].id, message, function(response)
+		{
+			if(response)
+			{
+				document.getElementById("ptitle").innerText=response;
+			}
+			else
+			{
+				document.getElementById("ptitle").innerText='脚本未注入,请重载(刷新)页面';
+			}
+		});
+	});
+}
+
+var selc_video=0;//视频序号
+var selc_playbackrate=1;//倍速
+function cvs()
+{
+    sendMessageToContentScript({cmdt:'video play back rate',vdid:selc_video,prt:selc_playbackrate});
+}
+//bind js func
+document.getElementById("ul_auto").onclick=()=>{
+	sendMessageToContentScript({cmdt:'ulearning auto play'});
+};
+document.getElementById("wlrz").onclick=()=>{
+	chrome.tabs.create({url:"http://172.31.252.91"});
+	//sendMessageToContentScript({cmdt:'jump to dgut wlrz'});
+};
+document.getElementById("yqdk_jump").onclick=()=>{
+	chrome.tabs.create({url:"https://yqfk-daka.dgut.edu.cn/"});
+	//sendMessageToContentScript({cmdt:'jump to dgut yqdk'});
+};
+document.getElementById("yqdk_auto").onclick=()=>{
+	sendMessageToContentScript({cmdt:'yqdk auto finish'});
+};
+document.getElementById("ymym").onclick=()=>{
+	chrome.tabs.create({url:"https://github.com/IsukaChiyogami/Azusa_edge_ext/"});
+	//sendMessageToContentScript({cmdt:'source code page'});
+};
+document.getElementById("vdidselc").onchange=()=>{
+	selc_video=Number(document.getElementById("vdidselc").value);
+};
+document.getElementById("vpbrselc").onchange=()=>{
+	selc_playbackrate=Number(document.getElementById("vpbrselc").value);
+};
+document.getElementById("change_vpbr").onclick=cvs;
---
+++ w_plugin_2.x.x/web_code/scr_winc.js
@@ -0,0 +1,183 @@
+ul_control=0;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+    if(request.cmdt=='video play back rate')
+    {
+        if(request.prt>0 && request.prt<=16)
+        {
+            const videoele=document.getElementsByTagName("video");
+            if(videoele)
+            {
+                if(videoele.length>request.vdid)
+                {
+                    videoele[request.vdid].playbackRate=request.prt;
+                    sendResponse("处理完成");
+                }
+                else
+                {
+                    sendResponse("超过总原声视频数量(原生共"+videoele.length+"个)");
+                }
+            } 
+            else
+            {
+                sendResponse("未找到原生视频");
+            }
+        }
+        else
+        {
+            sendResponse("倍速设置应在(0,16]");
+        }
+    }
+    else if(request.cmdt=='ulearning auto play')
+    {
+        Notification.requestPermission(function () {
+            if (Notification.permission === 'granted') {
+                // 用户点击了允许
+                let n = new Notification('开始执行', {
+                    body: '每章节完成后停止并提示'
+                })
+            }
+        })
+        const vdele=document.getElementsByTagName('video')[0];
+        if(vdele)
+        {
+            vdele.volume=0;//音量软性置零
+            vdele.playbackRate=16;//16倍速
+            pv=0;
+            ul_control=setInterval(vcheck,1000);
+            sendResponse("完成,每次运行自动过完本章节所有视频,非视频跳过,章节结束自动停止");
+        }
+        else
+        {
+            sendResponse("一个视频都没有哦,请从有视频的章节开始");
+        }
+    }
+    else if(request.cmdt=='jump to dgut wlrz')
+    {
+        sendResponse("完成,如果更换网址此方法将会失灵,可qq作者催更");
+        location.href="http://172.31.252.91";
+    }
+    else if(request.cmdt=='jump to dgut yqdk')
+    {
+        sendResponse("完成,如果更换网址此方法将会失灵,可qq作者催更");
+        location.href="https://yqfk-daka.dgut.edu.cn/";
+    }
+    else if(request.cmdt=='yqdk auto finish')
+    {
+        sendResponse("已开始,错误请报告qq3518767065");
+        yqdk_func();
+        
+    }
+    else if(request.cmdt=='source code page')
+    {
+        sendResponse("完成");
+        location.href="https://github.com/IsukaChiyogami/Azusa_edge_ext/";
+    }
+    else
+    {
+        sendResponse("错误:未知命令")
+    }
+    return true;
+});
+
+var pv=0;//播放中的视频序数
+
+function vcheck()
+{
+    const vdle=document.getElementsByTagName("video")[pv];
+    if(vdle)
+    {
+        if(vdle.paused)
+        {
+            if(document.getElementsByClassName("water")[0].clientHeight==14)//标识已完成则跳
+            {
+                console.log(document.getElementsByClassName("water")[0].clientHeight);
+                pv++;
+                if(document.getElementsByTagName("video").length==pv)
+                {
+                    pv=0;
+                    document.getElementsByClassName('next-page-btn cursor')[0].click();
+                }
+            }
+            else
+            {
+                vdle.play();
+            }
+        }
+        vdle.playbackRate=16;
+        vdle.volume=0;
+    }
+    else
+    {
+        document.getElementsByClassName('next-page-btn cursor')[0].click();
+        setTimeout(() => {
+            if(document.getElementsByClassName("stat-page chapter-stat complete").length || document.getElementById("alertModal").style.display=='block')
+            {
+                clearInterval(ul_control);
+                Notification.requestPermission(function () {
+                    if (Notification.permission === 'granted') {
+                        let n = new Notification('自动程序停止', {
+                            body: '章节完成或其他情况 ,请手动处理'
+                        })
+                    }
+                })
+            }
+        }, 100);
+    }
+}
+
+function yqdk_func()
+{
+    document.getElementsByClassName("select van-cell van-cell--clickable van-field")[0].click();//点击身体状况
+    setTimeout(() => {
+        document.getElementsByClassName("van-picker__confirm")[0].click();//确认身体状况
+
+        //体温
+        var ile = document.getElementsByClassName("van-field__control")[1]
+        var evt = new InputEvent('input', {
+            inputType: 'insertText',
+            dataTransfer: null,
+            isComposing: false
+        });
+        ile.value = '36.6';
+        ile.dispatchEvent(evt);
+
+        setTimeout(() => {
+            document.getElementsByClassName("select van-cell van-cell--clickable van-field")[1].click();//点击是否在校
+            setTimeout(() => {
+                document.getElementsByClassName("van-picker__confirm")[1].click();//确认是否在校
+                setTimeout(() => {
+                    document.getElementsByClassName("select van-cell van-cell--clickable van-field")[2].click();//选择校内住址
+                    setTimeout(() => {
+                        document.getElementsByClassName("van-cascader__option")[0].click()//选择学生公寓
+                        setTimeout(() => {
+                            document.getElementsByClassName("van-cascader__option")[6].click()//选择莞华12-15栋
+                            setTimeout(() => {
+                                document.getElementsByClassName("van-button van-button--info van-button--normal van-button--round")[0].click()//提交
+                                setTimeout(() => {
+                                    if(document.getElementsByClassName("van-button van-button--info van-button--normal van-button--round")[0].innerText=="提交")
+                                    {
+                                        document.getElementsByClassName("van-button van-button--info van-button--normal van-button--round")[0].click()//提交
+                                        setTimeout(() => {
+                                            if(document.getElementsByClassName("van-button van-button--info van-button--normal van-button--round")[0].innerText=="提交")
+                                            {
+                                                Notification.requestPermission(function () {
+                                                    if (Notification.permission === 'granted') {
+                                                        let n = new Notification('错误', {
+                                                            body: '打卡出现未知错误 ,请手动处理'
+                                                        })
+                                                    }
+                                                })
+                                            }
+                                        }, 1000);
+                                    }
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
